f164c21e236e1aaa48fddac18b691abfebd2f507
huynh <2051010119huynh@ou.edu.vn>
2024-02-21 22:13:20 +0700

    fix bug ui

--- src/Compoments/Navbar/Navbar.jsx
+++ src/Compoments/Navbar/Navbar.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import './Navbar.css'
 import navlogo from '../Assets/nav-logo.svg'
-import navProfile from '../Assets/nav-profile.svg'
 
 const Navbar = () => {
   return (
     <div className='navbar'>
         
         <img src={navlogo} alt='logo' />
-        <img src={navProfile} alt="" className='nav-profile'/>
+        <div></div>
     </div>
   )
 }
--- src/Compoments/Sidebar/Sidebar.jsx
+++ src/Compoments/Sidebar/Sidebar.jsx
@@ -2,20 +2,21 @@ import React from 'react'
 import './Sidebar.css'
 import { Link } from 'react-router-dom'
 import add_product_icon from '../Assets/Product_Cart.svg';
-import { product_list_icon } from '../Assets/product_list_icon.svg';
+import { SlNotebook } from "react-icons/sl";
+import { SlPlus } from "react-icons/sl";
 
 const Sidebar = () => {
   return (
     <div className='sidebar'>
       <Link to={'/addproduct'} style={{textDecoration:"none"}}>
         <div className="sidebar-item">
-            <img src={add_product_icon} alt="" />
+            <SlPlus className='text-normal'/>
             <p className="">Add Product</p>
         </div>
       </Link>
       <Link to={'/listproduct'} style={{textDecoration:"none"}}>
         <div className="sidebar-item">
-            <img src={product_list_icon} alt="" />
+            <SlNotebook className='text-normal'/>
             <p className="">Product List</p>
         </div>
       </Link>
